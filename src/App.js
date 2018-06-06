71635a5ae1848ea360d3eaf23eb99b1cfe44c018
jiaxin <edison7500@gmail.com>
2018-06-06 17:59:03 +0800

    merge

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import GitHub from "./components/github";
+import NotFound from "./components/NotFound";
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={GitHub} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
   }
 }
 
