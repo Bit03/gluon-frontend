b4dab76c5b54ecbbef7421841401932b5ff3def0
jielong <heyliudiao@gmail.com>
2018-06-29 10:29:32 +0800

    update webpack prod config

--- src/containers/HomePage/DAppList/CardList/index.js
+++ src/containers/HomePage/DAppList/CardList/index.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 import { Wrapper } from './styles';
 import Item from './Item';
+import defaultImg from '@static/images/default-big-gray.png';
 
 export default class CardList extends Component{
 
     renderItem = () => {
         let { data, isCard } = this.props;
-        let defaultImg = require('@static/images/default-big-gray.png');
+        // let defaultImg = require('@static/images/default-big-gray.png');
 
         return data.map((item, index) => {
             let img = item.site && item.site.logo_url ? item.site.logo_url : defaultImg;
